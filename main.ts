cd24436c1fac565929cd5ea9d7843692e34ef632
Nguyen Duc Duy <97716440+h8rdbrigde@users.noreply.github.com>
2025-03-26 02:09:11 +0700

    Updated

---
+++ main.ts
@@ -0,0 +1,19 @@
+
+//% weight=100 color=#0b5394 icon="" block="AI Camera"
+namespace AICamera {
+    //% block="Khởi tạo Camera AI với RX %rx TX %tx" weight=90
+    export function init(rx: SerialPin, tx: SerialPin): void {
+        serial.redirect(rx, tx, BaudRate.BaudRate9600);
+    }
+
+    //% block="Cập nhật dữ liệu từ Camera AI" weight=80
+    export function update(): void {
+        serial.writeString("update");
+    }
+
+    //% block="Kiểm tra kết quả AI" weight=70
+    export function checkResult(): string {
+        return serial.readUntil("
+");
+    }
+}
